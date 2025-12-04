09624e23b57a3b73988e51d474a902d869729cef
Angrymass <massimilianospagna@outlook.it>
2025-12-04 22:01:11 +0100

    modified:   Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from PyQt6 import QtGui, QtCore
+import sys
+class MainWindow(QtGui.QWindow):
+      def __init__(self):
+              QtGui.QWindow.__init__(self)
+              self.resize(350, 250)
+              self.setTitle('MainWindow')
+app = QtGui.QGuiApplication(sys.argv)
+main = MainWindow()
+main.show()
+sys.exit(app.exec())
