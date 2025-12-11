9d799e0b70e48247b54da8cbdd3cff779ad72314
Angrymass <massimilianospagna@outlook.it>
2025-12-11 22:49:05 +0100

    WACS Alpha v0.1 First Alpha Release

--- Main Window Manager.py
+++ Main Window Manager.py
@@ -3,7 +3,9 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QLa
 
 crono_voti = "Nessun Voto"
 media = "Nessun Voto"
+matmedia = "Nessuna Materia"
 
+crono_materie = {}
 listvoti = []
 
 class MainWindow(QMainWindow):
@@ -23,7 +25,7 @@ class MainWindow(QMainWindow):
 		self.aggvoto = QPushButton("Aggiungi Voto...")
 		self.aggvoto.clicked.connect(self.nvoto)
 
-		self.matmedia = QLabel("Medie Materie")
+		self.matmedia = QLabel(matmedia)
 
 		layout = QGridLayout()
 		layout.addWidget(self.voti, 0, 0)
@@ -36,9 +38,19 @@ class MainWindow(QMainWindow):
 		self.setCentralWidget(widget)
 		self.aggiorna()
 
+	def nvoto(self):
+		val = float(input("Voto: "))
+		mat = input("Materia: ")
+		listvoti.append([val, mat])
+		if mat not in crono_materie:
+			crono_materie[mat] = []
+		crono_materie[mat].append(val)
+		self.aggiorna()
+
 	def aggiorna(self):
 		self.make_crono_voti()
 		self.aggmedia()
+		self.aggmatmedia()
 
 	def aggmedia(self):
 		if len(listvoti) != 0:
@@ -50,12 +62,6 @@ class MainWindow(QMainWindow):
 			media = "Media: %s" %media
 			self.totmedia.setText(media)
 
-	def nvoto(self):
-		val = float(input("Voto: "))
-		mat = input("Materia: ")
-		listvoti.append([val, mat])
-		self.aggiorna()
-
 	def make_crono_voti(self):
 		global crono_voti
 		if len(listvoti) != 0:
@@ -63,6 +69,16 @@ class MainWindow(QMainWindow):
 			for i in range(0, len(listvoti)):
 				crono_voti += "%s - %s\n" %(listvoti[i][0], listvoti[i][1])
 		self.voti.setText(crono_voti)
+
+	def aggmatmedia(self):
+		global matmedia
+		if len(crono_materie) != 0:
+			matmedia = "Medie Materie:\n"
+			for materia, voti in crono_materie.items():
+				sommavoti = sum(voti)
+				mediamat = round((sommavoti / len(voti)), 2)
+				matmedia += "%s: %s\n" %(materia, mediamat)
+			self.matmedia.setText(matmedia)
 			
 
 app = QApplication(sys.argv)
